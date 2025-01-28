8fec031ebc3563adb97408186819afad93cba00f
Zaeem Khan <111380015+Zaeem2141@users.noreply.github.com>
2025-01-28 15:30:58 +0500

    Time: 0 ms (100%), Space: 8.5 MB (33.28%) - LeetHub

--- 3636-check-balanced-string/3636-check-balanced-string.cpp
+++ 3636-check-balanced-string/3636-check-balanced-string.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool isBalanced(string num) {
-        int sum=0, len=num.size();
-        for(int i=1 ; i< len; i+=2){
+        int sum=0;
+        for(int i=1 ; i< num.size(); i+=2){
           sum+=num[i]-'0';
           sum-=num[i-1]-'0';
         }
-        if(len %2 != 0){
-          sum-=num[len-1]-'0';
+        if(num.size() %2 != 0){
+          sum-=num[num.size()-1]-'0';
         }
         return sum==0;
     }
