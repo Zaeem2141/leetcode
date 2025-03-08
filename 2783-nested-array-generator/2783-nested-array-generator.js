c4b0372a9a8face8e278f2c312539e768cdefb04
Zaeem Khan <111380015+Zaeem2141@users.noreply.github.com>
2025-03-09 02:53:38 +0500

    Time: 84 ms (98.35%), Space: 73 MB (84.06%) - LeetHub

---
+++ 2783-nested-array-generator/2783-nested-array-generator.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+var inorderTraversal = function*(arr) {
+       for (const item of arr.flat(Infinity)) {
+        yield item;
+    }
+};
+
+/**
+ * const gen = inorderTraversal([1, [2, 3]]);
+ * gen.next().value; // 1
+ * gen.next().value; // 2
+ * gen.next().value; // 3
+ */
