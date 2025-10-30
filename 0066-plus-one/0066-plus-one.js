54775d01a455c10c0d34394ede4bce811c8d0ee9
Zaeem Khan <111380015+Zaeem2141@users.noreply.github.com>
2025-10-30 17:32:01 +0500

    Time: 0 ms (100%), Space: 53.7 MB (79.34%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+  // If all digits were 9, you need to add one more digit at the start
+  digits.unshift(1);
+  return digits;
+};
