29fb51b1d7e6e95e842f4435effdd74421fe29ed
Zaeem Khan <111380015+Zaeem2141@users.noreply.github.com>
2026-01-12 19:36:07 +0500

    Time: 3 ms (27.98%), Space: 58.2 MB (6.1%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    let i = a.length-1, j = b.length-1, carry=0, result=''
+
+    while(i>=0 || j>=0 || carry){
+      let sum = carry
+      i>=0 && (sum += +a[i--])
+      j>=0 && (sum += +b[j--])
+      result = (sum % 2  ) +result
+      carry = Math.floor(sum / 2);
+    }
+
+    return result
+};
